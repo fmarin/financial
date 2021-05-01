0413d142cf4630642ff5f854a5d98441ac42934e
Frank MF <frank.mf@test.dev>
2021-05-01 18:50:01 +0200

    Add PhpUnit

--- apps/backoffice/backend/public/index.php
+++ apps/backoffice/backend/public/index.php
@@ -4,7 +4,7 @@ use Financial\Apps\Backoffice\Backend\BackofficeBackendKernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/../bootstrap.php';
+require dirname(__DIR__) . '/../../bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
