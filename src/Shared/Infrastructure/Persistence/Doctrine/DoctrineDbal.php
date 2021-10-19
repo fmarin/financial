9e306cf37c526cc0d339026c1f20be314e86e40e
Frank Marin <frank.marin@digimobil.es>
2021-10-19 21:55:09 +0200

    Update Shared BC

--- src/Shared/Infrastructure/Persistence/Doctrine/DoctrineDbal.php
+++ src/Shared/Infrastructure/Persistence/Doctrine/DoctrineDbal.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace Financial\Shared\Infrastructure\Persistence\Doctrine;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Driver\ResultStatement;
 
 abstract class DoctrineDbal
 {
@@ -15,8 +16,31 @@ abstract class DoctrineDbal
         $this->connection = $connection;
     }
 
+    protected function connection(): Connection
+    {
+        return $this->connection;
+    }
+
     public function fetchAll($sql, $params = []): array
     {
         return $this->connection->fetchAll($sql, $params);
+    }
+
+    public function executeUpdate($sql, $params = [])
+    {
+        try {
+            return $this->connection->executeUpdate($sql, $params);
+        } catch (\Exception $exception) {
+            return $exception;
+        }
+    }
+
+    public function executeQuery($sql, $params = [])
+    {
+        try {
+            return $this->connection->executeQuery($sql, $params);
+        } catch (\Exception $exception) {
+            return $exception;
+        }
     }
 }
